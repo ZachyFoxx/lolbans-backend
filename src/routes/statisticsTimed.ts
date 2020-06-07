29277154c3d2a6a030f8c24e7374b76229a0088c
skyefoxie <actuallyori@gmail.com>
2020-06-08 00:51:25 +0100

    routes: Use unix timestamps for statistics/timed

--- src/routes/statisticsTimed.ts
+++ src/routes/statisticsTimed.ts
@@ -18,22 +18,44 @@ export const statisticsTimed = (server: ApiServer) => async (
     res: Response
 ) => {
     const start = req.query.start
-        ? moment.default(req.query.start as string)
-        : moment.default().subtract(6, "days");
+        ? moment.default.unix(
+              (isNaN(Number(req.query.start))
+                  ? Date.now()
+                  : Number(req.query.start)) / 1000
+          )
+        : moment.default().subtract(7, "days");
 
     const end = req.query.end
-        ? moment.default(req.query.end as string)
+        ? moment.default.unix(
+              (isNaN(Number(req.query.end))
+                  ? Date.now()
+                  : Number(req.query.end)) / 1000
+          )
         : moment.default();
 
     // bad >:C
-    if (end.isSameOrBefore(start)) {
+    if (end.isSameOrBefore(start, "days")) {
+        server.logger.debug(
+            "Rejecting request - start date is after end date."
+        );
         return badRequest(req, res);
     }
 
     const data = [];
     const range = moment.range(start, end);
+
+    if (range.diff("days") > 30) {
+        server.logger.debug(
+            `Rejecting request - exceeds maximum date difference (${range.diff(
+                "days"
+            )}).`
+        );
+        return badRequest(req, res);
+    }
+
     const repo = server.connection.getRepository(Punishment);
 
+    // Find a way to speed this up without overloading the database.
     for (const m of range.by("day", { excludeEnd: false })) {
         const date = m.toDate();
         const [bans, kicks, mutes, warns] = await Promise.all([
@@ -72,6 +94,8 @@ export const statisticsTimed = (server: ApiServer) => async (
             total: bans + kicks + mutes + warns,
         });
     }
+
+    server.logger.debug(`Pulled ${data.length} dates.`);
 
     return res.json(data);
 };
