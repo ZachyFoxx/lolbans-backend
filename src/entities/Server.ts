54c9b96367c6e776aac43ddbc4c7ac2720fca8ec
SkyezerFox <actuallyori@gmail.com>
2020-07-30 13:46:28 +0100

    primarily a commit to make sure my new gpg key works

--- src/entities/Server.ts
+++ src/entities/Server.ts
@@ -1,4 +1,6 @@
+import { createECDH, generateKeyPair, generateKeyPairSync } from "crypto";
 import { Request, Response } from "express";
+import * as jwt from "jsonwebtoken";
 import { Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuid } from "uuid";
 
@@ -24,6 +26,34 @@ export class Server {
      */
     static async createServer(server: ApiServer, req: Request, res: Response) {
         // Do some sort of cryptography here
+
+        const id = uuid();
+
+        server.logger.verbose(
+            `Generating public/private key pair for new server '${id}'...`
+        );
+
+        const keypair = generateKeyPairSync("ec", { namedCurve: "secp128r2" });
+
+        server.logger.debug("Generated keypairs using ECDH curve secp128r2");
+
+        const token = jwt.sign(
+            { id },
+            keypair.privateKey.export({ type: "sec1", format: "pem" }),
+            {
+                algorithm: "ES512",
+            }
+        );
+
+        server.logger.debug(token);
+
+        const decoded = jwt.verify(
+            token,
+            keypair.publicKey.export({ type: "pkcs1", format: "pem" })
+        );
+
+        server.logger.debug(decoded.toString());
+
         return new Server();
     }
 }
